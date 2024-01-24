b71ad6a11369e1a9d49c228fb5cdab1ee3401b62
thainv26 <thainvph34472@fpt.edu.vn>
2024-01-24 13:25:36 +0700

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import ProductItem from "./components/ProductItem";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+// React es7+... : gõ câu lệnh rafce để tạo component
+// cài đặt extension: html to jsx
+
+const productList = [
+    { name: "Product A", price: 200 }, // product
+    { name: "Product B", price: 300 }, // product
+    { name: "Product C", price: 400 }, // product
+];
+function App() {
+    return (
+        <div>
+            {
+                
+            }
+            {productList.map((product) => {
+                return <ProductItem key={product.name} product={product} />;
+            })}
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
